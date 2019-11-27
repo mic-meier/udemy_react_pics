7b8b8c4f3f5904932d23adcdc1245778975ae416
mic-meier <mic.l.meier@gmail.com>
2019-11-27 22:36:17 +0100

    89 + 90 axios request and viewing request results

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID ebd40994e8c2bb36cece028ff32b8ac13f22a06496e097d2fdd85ce23c5c5923"
+      }
+    });
   }
 
   render() {
