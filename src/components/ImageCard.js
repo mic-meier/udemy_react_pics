fdf8b28e12008fb012305a37ae628f0ef29e932c
mic-meier <mic.l.meier@gmail.com>
2019-11-29 14:36:57 +0100

    102 image card component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ImageCard extends React.Component {
+  render() {
+    const { alt_description, urls } = this.props.image;
+
+    return (
+      <div>
+        <img src={urls.regular} alt={alt_description} />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
